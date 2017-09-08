6bf8752157cfdc1a6423b70d3253a0e2bf2b40dd
sergioesmerio <Yorick07>
2017-09-08 12:26:29 -0300

    added function to read n rounds of the league in loop

--- classificacao.py
+++ classificacao.py
@@ -5,30 +5,30 @@ rounds = {}
 
 points = {}  # key = team  value = [total points, points as host, points as guest]
 
-plays = {}    # key = team value = [total plays, plays as host, plays as guest]
-
+plays = {}  # key = team value = [total plays, plays as host, plays as guest]
 
 performs = {}
-no_goals_received = {} # games that team suffer no goals: total, as host, as guest
+no_goals_received = {}  # games that team suffer no goals: total, as host, as guest
 top_goals_scored = {}
 goals_scored = {}
 goals_suffered = {}
 idx_goals_attack = {}
 idx_goals_defense = {}
 
+
 def create_team_list(ref_round, team_list):
     for match in ref_round:
         team_list[match["host"]] = [0, 0, 0]
         team_list[match["guest"]] = [0, 0, 0]
-        #plays[match["host"]] = [0, 0, 0]
-        #plays[match["guest"]] = [0, 0, 0]
+        # plays[match["host"]] = [0, 0, 0]
+        # plays[match["guest"]] = [0, 0, 0]
 
 
 def calculate_performance():
     for team in plays.keys():
         performs[team] = [0, 0, 0]
         for i in range(3):
-            performs[team][i] = points[team][i]/(plays[team][i]*3)
+            performs[team][i] = points[team][i] / (plays[team][i] * 3)
 
 
 def count_no_goals_received():
@@ -59,20 +59,20 @@ def calc_idx_goals_attack(match_round):
     if idx_goals_attack == {}:
         create_team_list(rounds["1"], idx_goals_attack)
     for match in match_round:
-        idx_goals_attack[match["host"]][0] = goals_scored[match["host"]][1]/plays[match["host"]][1] * \
-                                             goals_suffered[match["guest"]][2]/plays[match["guest"]][2]
-        idx_goals_attack[match["guest"]][0] = goals_scored[match["guest"]][2]/plays[match["guest"]][2] * \
-                                              goals_suffered[match["host"]][1]/plays[match["host"]][1]
+        idx_goals_attack[match["host"]][0] = goals_scored[match["host"]][1] / plays[match["host"]][1] * \
+                                             goals_suffered[match["guest"]][2] / plays[match["guest"]][2]
+        idx_goals_attack[match["guest"]][0] = goals_scored[match["guest"]][2] / plays[match["guest"]][2] * \
+                                              goals_suffered[match["host"]][1] / plays[match["host"]][1]
 
 
 def calc_idx_goals_defense(match_round):
     if idx_goals_defense == {}:
         create_team_list(rounds["1"], idx_goals_defense)
     for match in match_round:
-        idx_goals_defense[match["host"]][0] = 1 / ((goals_suffered[match["host"]][1]/plays[match["host"]][1]) *
-                                                   (goals_scored[match["guest"]][2])/plays[match["guest"]][2])
-        idx_goals_defense[match["guest"]][0] = 1 / ((goals_suffered[match["guest"]][2]/plays[match["guest"]][2]) *
-                                                    (goals_scored[match["host"]][1]/plays[match["host"]][1]))
+        idx_goals_defense[match["host"]][0] = 1 / ((goals_suffered[match["host"]][1] / plays[match["host"]][1]) *
+                                                   (goals_scored[match["guest"]][2]) / plays[match["guest"]][2])
+        idx_goals_defense[match["guest"]][0] = 1 / ((goals_suffered[match["guest"]][2] / plays[match["guest"]][2]) *
+                                                    (goals_scored[match["host"]][1] / plays[match["host"]][1]))
 
 
 def count_goals_suffered():
@@ -84,7 +84,6 @@ def count_goals_suffered():
             goals_suffered[match["guest"]][2] += match["host score"]
             goals_suffered[match["host"]][0] += match["guest score"]
             goals_suffered[match["host"]][1] += match["guest score"]
-
 
 
 def count_top_scorer(top_score):
@@ -109,9 +108,9 @@ def calculate_teams_points():
     for match_round in rounds:
         for match in rounds[match_round]:
             plays[match["host"]][0] += 1
-            plays[match["host"]][1] += 1    #add play as host
+            plays[match["host"]][1] += 1  # add play as host
             plays[match["guest"]][0] += 1
-            plays[match["guest"]][2] += 1   #add play as guest
+            plays[match["guest"]][2] += 1  # add play as guest
             if match["host score"] == match["guest score"]:
                 points[match["host"]][0] += 1
                 points[match["guest"]][0] += 1
@@ -127,7 +126,7 @@ def calculate_teams_points():
 
 # ler_arquivo_resultados
 # function to load matches from files anda store on rounds collection
-def ler_arquivo_resultados(file_name):
+def read_round_results(file_name):
     try:
         rodada = open(file_name, 'r', encoding='utf8')
         header = rodada.readline().split(" ")
@@ -146,7 +145,6 @@ def ler_arquivo_resultados(file_name):
             match["guest score"] = int(score[1])
             match_round.append(match)
 
-
         rodada.close()
         rounds[round_num] = match_round
 
@@ -154,11 +152,16 @@ def ler_arquivo_resultados(file_name):
         print("Erro ao abrir arquivo:", file_name)
 
 
+def read_league_results(num_rounds):
+    for i in range(num_rounds):
+        read_round_results("resultados_rodada" + str(i + 1))
+
+
 def read_next_round_file(file_name):
     try:
         file = open(file_name, 'r', encoding='utf8')
         file.readline().split(" ")
-        #round_num = header[1].strip()
+        # round_num = header[1].strip()
         match_round = []
         for line in file:
             match = {}
@@ -180,7 +183,7 @@ def print_sorted_table(table):
     list_to_print = sorted(table.items(), key=itemgetter(1), reverse=True)
     pos = 1
     for item in list_to_print:
-        print(format(pos, "02n")+ " " + format(item[0], "12s") + " " + str(item[1]))
+        print(format(pos, "02n") + " " + format(item[0], "12s") + " " + str(item[1]))
         pos += 1
 
 
@@ -190,45 +193,23 @@ def save_classification():
         file_classif = open("classificacao_geral", 'w', encoding='utf8')
         pos = 1
         for item in list_to_save:
-            #print(format(pos, "02n")+ " " + format(item[0], "12s") + " " + str(item[1]) + " " + str(plays[item[0]]) +
+            # print(format(pos, "02n")+ " " + format(item[0], "12s") + " " + str(item[1]) + " " + str(plays[item[0]]) +
             #      " " + format(performs[item[0]][0],".3f"))
-            file_classif.write(format(pos, "02n")+ " " + format(item[0], "12s") + " " + str(item[1][0]) + " "
-                               + str(plays[item[0]][0])+ "\n")
+            file_classif.write(format(pos, "02n") + " " + format(item[0], "12s") + " " + str(item[1][0]) + " "
+                               + str(plays[item[0]][0]) + "\n")
             pos += 1
         file_classif.close()
     except FileExistsError:
         print("Erro ao salvar arquivo:", "classificacao_geral")
 
 
-
 def main():
-    ler_arquivo_resultados('resultados_rodada1')
-    ler_arquivo_resultados('resultados_rodada2')
-    ler_arquivo_resultados('resultados_rodada3')
-    ler_arquivo_resultados('resultados_rodada4')
-    ler_arquivo_resultados('resultados_rodada5')
-    ler_arquivo_resultados('resultados_rodada6')
-    ler_arquivo_resultados('resultados_rodada7')
-    ler_arquivo_resultados('resultados_rodada8')
-    ler_arquivo_resultados('resultados_rodada9')
-    ler_arquivo_resultados('resultados_rodada10')
-    ler_arquivo_resultados('resultados_rodada11')
-    ler_arquivo_resultados('resultados_rodada12')
-    ler_arquivo_resultados('resultados_rodada13')
-    ler_arquivo_resultados('resultados_rodada14')
-    ler_arquivo_resultados('resultados_rodada15')
-    ler_arquivo_resultados('resultados_rodada16')
-    ler_arquivo_resultados('resultados_rodada17')
-    ler_arquivo_resultados('resultados_rodada18')
-    ler_arquivo_resultados('resultados_rodada19')
-    ler_arquivo_resultados('resultados_rodada20')
-    ler_arquivo_resultados('resultados_rodada21')
-    ler_arquivo_resultados('resultados_rodada22')
+    # load results from the first n rounds of league
+    read_league_results(22)
 
-    #create_team_list(rounds["1"])
     calculate_teams_points()
 
-    #for line in teams:
+    # for line in teams:
     #    print(line, ":", teams[line])
     calculate_performance()
 
@@ -239,13 +220,17 @@ def main():
 
     save_classification()
 
-
     next_round = read_next_round_file('jogos_rodada23')
 
     calc_idx_goals_attack(next_round)
+    print("--Melhores times para escalar Atacantes:")
     print_sorted_table(idx_goals_attack)
 
+    print("\n")
+
     calc_idx_goals_defense(next_round)
+    print("--Melhores times para escalar Defensores:")
     print_sorted_table(idx_goals_defense)
+
 
 main()
