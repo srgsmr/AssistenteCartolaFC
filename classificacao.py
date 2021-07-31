4abc4464e1b93abf412414a063f4d7fb657c4202
srgsmr <srgsmr@gmail.com>
2021-07-31 11:54:43 -0300

    ordering positions by mean

--- classificacao.py
+++ classificacao.py
@@ -363,19 +363,18 @@ def main():
         coach_param = {"label": "TÉCNICOS", "code": 6, "idx": "idx_coach", "qty": 5, "qty_bk": 0}
         columns_to_print = ["abreviacao", "apelido", "pos_pts", "preco_num", "media_num", "roi", "status_id"]
 
+        team_list = []
         for param in [goalkeeper_param, attack_param, defense_param, midfield_param, sidefield_param, coach_param]:
-            print("* " + param["label"] + " ****************************************************************************")
             rank_players = cart.select_players(df_comp, param["code"], param["idx"])
             if param["code"] != 6:
                 coach_team.append(rank_players)
                 param["players"] = coach_team[-1].head(param["qty"])
-                print(param["players"][columns_to_print])
+                team_list.append((param["players"]["media_num"].mean(), param))
                 pre_team.append(param["players"])
-                print("----------------------banco--------------------------------------------------------------------")
                 lowest_price = min(param["players"]["preco_num"])
                 df2 = rank_players.drop(param["players"].index)
                 df3 = df2.drop(df2[df2.preco_num > lowest_price].index)
-                print(df3.head(param["qty_bk"])[columns_to_print])
+                param["subst"] = df3.head(param["qty_bk"])
 
             else:
                 # special calculation for coaches, must be the last iteration on this loop. Need all other players agregated
@@ -387,8 +386,18 @@ def main():
                 df_mean_team = df_pos.groupby("clube_id").mean(numeric_only=True)
                 rank_players = rank_players.join(df_mean_team, lsuffix="_coach", rsuffix="_groupby")
                 param["players"] = rank_players.sort_values("pos_pts_groupby", ascending=False).head(param["qty"])
-                print(param["players"][["abreviacao", "apelido",  "pos_pts_groupby", "preco_num", "media_num_groupby", "roi", "status_id"]])
+
+        team_list.sort(reverse=True)
+        for rank, param in team_list:
+            print("* " + param["label"] + " ****************************************************************** média = " + "{:.2f}".format(rank))
+            print(param["players"][columns_to_print])
+            print("- banco --------------------------------------------------------------------")
+            print(param["subst"][columns_to_print])
             print()
+        print("* " + coach_param["label"] + " **************************************************************************")
+        print(coach_param["players"][
+                  ["abreviacao", "apelido", "pos_pts_groupby", "preco_num", "media_num_groupby", "roi", "status_id"]])
+        print()
 
         print("CAPITÃES")
         df_selected = pd.concat(pre_team)
@@ -410,14 +419,6 @@ def main():
         print(df_captains.sort_values("pontos_num_median", ascending=False)[["team", "apelido",
                                                                                   "pontos_num_median"]].head(8))
 
-        # print("RESERVAS")
-        # df_substitutes = pd.concat(coach_team).set_index('atleta_id')
-        # for pos, qtd in [(1, 3), (5, 7), (3, 5), (4, 7), (2, 5)]:
-        #     df2 = df_substitutes.drop(df_substitutes[df_substitutes.posicao_id != pos].index)
-        #     lowest_price = min(df2.head(qtd)["preco_num"])
-        #     df2 = df2.drop(df2.head(qtd).index)
-        #     df3 = df2.drop(df2[df2.preco_num > lowest_price].index)
-        #     print(df3.head(1)[columns_to_print])
 
     else: # select players using price difference between the last and actual seasons
         print('Primeiras rodadas, vamos escolher os jogadores em relação ao preço que terminaram a temporada passada...')
